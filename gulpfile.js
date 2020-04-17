4bde0021d597bfb8734be216c5a9153ca827d218
tkoleary <terrenceoleary@gmail.com>
2020-04-17 14:05:32 -0500

    updated glupfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-// var autoprefixer = require('gulp-autoprefixer');
 
 sass.compiler = require('node-sass');
 
@@ -11,14 +10,6 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./library/css'));
 });
-
-/*gulp.task('prefix', function () {
-  return gulp.src('./library/css')
-    .pipe(autoprefixer({
-        cascade: false
-    ))
-    .pipe(gulp.dest('./library/css'))
-});*/
 
 gulp.task('sass:watch', function () {
   gulp.watch('./library/scss/**/*.scss', ['sass']);
